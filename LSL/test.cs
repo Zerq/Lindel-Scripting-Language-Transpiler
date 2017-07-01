ccd2c41cb8b2d95b7cf88a08295d5593b4778b76
Zerq <zerq_tm@hotmail.com>
2017-07-01 10:34:52 +0200

    ...

--- LSL/test.cs
+++ LSL/test.cs
@@ -4,6 +4,10 @@ using Codegen;
 
 namespace LSL
 {
+
+    public class test2 {
+        public static int testzor = 5;
+    }
     public class test
     {
         public static int testInt = 13;
@@ -14,7 +18,9 @@ namespace LSL
 
             Flow.State("default", () => {
               
-                var x = test.testint == 3;
+                var x = testint == 3;
+                test2.testzor = 4;
+                var b6x = test2.testzor == 4;
 
                 Flow.If(() => test.testint == 3, () => {
                     Math.llAbs(4);
